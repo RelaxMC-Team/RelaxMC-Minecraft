0e0317879ecf920b45828d636e1b9ca2f60abdc6
KitoMC <95675413+KitoMCVN@users.noreply.github.com>
2024-07-22 02:24:14 +0000

    .

--- CPP/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::string hello = "Hello World!";
-    
-    std::cout << hello << std::endl;
-    
-    return 0;
-}
--- Pascal/HelloWorld.pas
+++
@@ -1,9 +0,0 @@
-program HelloWorld;
-
-var
-  hello: string;
-
-begin
-  hello := 'Hello World!';
-  writeln(hello);
-end.
